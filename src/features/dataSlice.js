ff453e86f6f11e105c69abe885c878c7ea36e030
malfell <malfellows13@gmail.com>
2022-07-25 13:09:31 -0500

    thunk completed

--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -36,16 +36,31 @@ export const dataSlice = createSlice({
     }
 })
 
+// export dataSlice actions
 export const { setData, clearData, incrementId, decrementId, inputId } = dataSlice.actions
 
+// data thunk
+// 1 thunk action creator
 export const fetchData = () => {
-    const fetchDataThunk = async (dispatch, getState) => {
+    // 2 thunk function--must be async
+        // dispatch and getState as parameters
+    const dataThunk = async (dispatch, getState) => {
+        // 3 side effects
+        // need state too
         let state = getState()
+        // get response from URL--must use AWAIT
         const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${state.data.objectId}`)
-        const rData = await response.json()
-        dispatch(setData(rData))
+        // then await response and convert it to json
+        const resData = await response.json()
+        // console log to make sure it's successful
+        console.log(resData)
+        // 4 dispatch to pertinent render
+        dispatch(setData(resData))
+
     }
-    return fetchDataThunk
+    // then return the dataThunk
+    return dataThunk
 }
 
+// export dataSlice as reducer
 export default dataSlice.reducer
